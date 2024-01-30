c2ca0042227e9213508fde38f5edec84775c359f
Simone-Corso <scorso26@gmail.com>
2024-01-30 16:51:37 +0100

    ho iniziato a creare una pagina

---
+++ laravel-primi-passi/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $data = [
+        'title' => 'Benvenuto su Laravel Primi Passi Con Il Mitico Simone',
+        'content' => 'Questa è la tua prima pagina con Laravel!, Adesso Si Mangia La Pizza'
+    ];
+
+    return view('home', $data);
+});
